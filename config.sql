8dc30c643ef84cef5340fd9ce43ab12b23350b10
DavidDevGt <josuedavidvl18@gmail.com>
2023-10-22 13:09:30 -0600

    BBDD NORMALIZADA Y MEJORADA

--- config.sql
+++ config.sql
@@ -126,7 +126,7 @@ CREATE TABLE marcas (
     active TINYINT(1) DEFAULT 1
 );
 
--- Tabla de productos con campos adicionales
+-- Tabla de productos
 CREATE TABLE productos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     codigo VARCHAR(10) UNIQUE,
@@ -141,32 +141,63 @@ CREATE TABLE productos (
     categoria_id INT NOT NULL,
     proveedor_id INT NOT NULL,
     marca_id INT,
+    unidad_medida_id INT,
     active TINYINT(1) DEFAULT 1,
     FOREIGN KEY (categoria_id) REFERENCES categorias(id),
     FOREIGN KEY (proveedor_id) REFERENCES proveedores(id),
-    FOREIGN KEY (marca_id) REFERENCES marcas(id)
+    FOREIGN KEY (marca_id) REFERENCES marcas(id),
+    FOREIGN KEY (unidad_medida_id) REFERENCES unidades_medida(id)
+);
+
+-- Tabla de unidades de medida
+CREATE TABLE unidades_medida (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL UNIQUE,
+    descripcion TEXT,
+    active TINYINT(1) DEFAULT 1
 );
 
 -- Tabla de clientes
 CREATE TABLE clientes (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    nit VARCHAR(15) NOT NULL UNIQUE,
     nombre VARCHAR(255) NOT NULL,
     apellido VARCHAR(255) NOT NULL,
-    direccion TEXT NOT NULL,
-    telefono VARCHAR(20) NOT NULL,
+    direccion_facturacion TEXT NOT NULL,
+    direccion_entrega TEXT NOT NULL,
+    ciudad VARCHAR(255),
+    departamento VARCHAR(255),
+    pais VARCHAR(255) DEFAULT 'Guatemala',
+    telefono VARCHAR(20),
     email VARCHAR(255) UNIQUE,
+    tipo_cliente ENUM('Individual', 'Corporativo') DEFAULT 'Individual',
     fecha_registro DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     active TINYINT(1) DEFAULT 1
+);
+
+-- Tabla de sucursales de clientes
+CREATE TABLE sucursales (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_id INT NOT NULL,
+    nit_sucursal VARCHAR(15),
+    nombre_sucursal VARCHAR(255) NOT NULL,
+    direccion_sucursal TEXT NOT NULL,
+    ciudad_sucursal VARCHAR(255),
+    departamento_sucursal VARCHAR(255),
+    telefono_sucursal VARCHAR(20),
+    email_sucursal VARCHAR(255),
+    active TINYINT(1) DEFAULT 1,
+    FOREIGN KEY (cliente_id) REFERENCES clientes(id)
 );
 
 -- Tabla de empleados
 CREATE TABLE empleados (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
-    apellido VARCHAR(255) NOT NULL,
-    direccion TEXT NOT NULL,
+    apellido VARCHAR(255),
+    direccion TEXT,
     telefono VARCHAR(20) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) UNIQUE,
     fecha_contratacion DATE NOT NULL,
     rol_id INT,
     active TINYINT(1) DEFAULT 1,
@@ -177,10 +208,16 @@ CREATE TABLE empleados (
 CREATE TABLE pedido (
     id INT AUTO_INCREMENT PRIMARY KEY,
     cliente_id INT NOT NULL,
+    sucursal_id INT,
     fecha_pedido DATE NOT NULL,
-    estado ENUM('Pendiente', 'Completado', 'Cancelado') NOT NULL,
+    fecha_estimada_entrega DATE,
+    estado ENUM('Creado', 'Pendiente', 'Preparando', 'En Revisión', 'Entregado', 'Completado', 'Cancelado') NOT NULL,
+    total DECIMAL(10,2) NOT NULL,
+    metodo_envio ENUM('Guatex', 'Recogen en Local', 'Traslado a Bodega', 'Otro'),
+    observaciones_pedido TEXT,
     active TINYINT(1) DEFAULT 1,
-    FOREIGN KEY (cliente_id) REFERENCES clientes(id)
+    FOREIGN KEY (cliente_id) REFERENCES clientes(id),
+    FOREIGN KEY (sucursal_id) REFERENCES sucursales(id)
 );
 
 -- Detalles de pedidos
@@ -197,10 +234,15 @@ CREATE TABLE pedido_d (
 -- Tabla de facturas
 CREATE TABLE factura (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    pedido_id INT,
     cliente_id INT NOT NULL,
     fecha_factura DATE NOT NULL,
+    serie VARCHAR(10),
+    numero_factura INT,
+    autorizacion VARCHAR(255),
     total DECIMAL(10,2) NOT NULL,
-    FOREIGN KEY (cliente_id) REFERENCES clientes(id)
+    FOREIGN KEY (cliente_id) REFERENCES clientes(id),
+    FOREIGN KEY (pedido_id) REFERENCES pedido(id)
 );
 
 -- Detalles de facturas
@@ -259,8 +301,13 @@ CREATE TABLE movimientos_inventario (
     cantidad INT NOT NULL,
     tipo ENUM('Ingreso', 'Salida', 'Ajuste') NOT NULL,
     fecha_movimiento DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    motivo TEXT NOT NULL,
-    FOREIGN KEY (producto_id) REFERENCES productos(id)
+    motivo TEXT,
+    orden_compra_id INT,
+    factura_id INT,
+    ajuste_id INT,
+    FOREIGN KEY (producto_id) REFERENCES productos(id),
+    FOREIGN KEY (orden_compra_id) REFERENCES orden_compra(id),
+    FOREIGN KEY (factura_id) REFERENCES factura(id)
 );
 
 -- Tabla para gestionar ingresos de inventario
@@ -302,4 +349,54 @@ CREATE TABLE historial_precios (
     precio_venta_anterior DECIMAL(10,2),
     fecha_cambio DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (producto_id) REFERENCES productos(id)
+);
+
+
+-- Tablas auxiliares por si crece la ferretería, de momento las creo pero no las uso mucho
+
+-- Tabla de unidades de mayoreo (fardo, caja, palet, etc.)
+CREATE TABLE unidades_mayoreo (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tipo VARCHAR(255) NOT NULL, -- Ejemplo: Fardo, Caja, Palet o Contenedor
+    cantidad_unidades INT NOT NULL, -- Ejemplo: 1000 tornillos en un fardo
+    producto_id INT NOT NULL,
+    fecha_compra DATE NOT NULL,
+    active TINYINT(1) DEFAULT 1,
+    FOREIGN KEY (producto_id) REFERENCES productos(id)
+);
+
+-- Tabla de logística y envío
+CREATE TABLE logistica_envio (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_agencia VARCHAR(255) NOT NULL,
+    tiempo_estimado_envio VARCHAR(255),
+    costo DECIMAL(10, 2),
+    observaciones TEXT
+);
+
+-- Tabla de ubicaciones de producto dentro de un almacén
+CREATE TABLE ubicaciones_almacen (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    producto_id INT NOT NULL,
+    almacen_id INT NOT NULL,
+    cantidad DECIMAL(10, 2) NOT NULL,
+    estante VARCHAR(50),
+    fila VARCHAR(50),
+    columna VARCHAR(50),
+    FOREIGN KEY (producto_id) REFERENCES productos(id),
+    FOREIGN KEY (almacen_id) REFERENCES almacenes(id)
+);
+
+-- Tabla de almacenes o bodegas
+CREATE TABLE almacenes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL UNIQUE,
+    direccion TEXT,
+    ciudad VARCHAR(255),
+    departamento VARCHAR(255),
+    pais VARCHAR(255) DEFAULT 'Guatemala',
+    telefono VARCHAR(20),
+    capacidad_maxima DECIMAL(10, 2),
+    capacidad_actual DECIMAL(10, 2) DEFAULT 0,
+    active TINYINT(1) DEFAULT 1
 );
